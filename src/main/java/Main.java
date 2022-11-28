b7c027804c1b34706ab002a446e7d724df606e76
jebavna1 <NiceTryx@gmail.com>
2022-11-28 13:46:43 +0100

    test reflexe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,10 @@ import models.chatClients.fileOperations.JsonChatFileOperations;
 import models.database.DbInitializer;
 import models.gui.MainFrame;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -18,6 +22,18 @@ public class Main {
 
         ChatFileOperations chatFileOperations = new JsonChatFileOperations();
         ChatClient chatClient = new ApiChatClient();
+
+        //region Test Reflexe
+        Class<ApiChatClient> refExample = ApiChatClient.class;
+        List<Field> fields = getAllFields(refExample);
+
+        System.out.println("Class name - " + refExample.getName());
+        for(Field f : fields)
+        {
+            System.out.println(f.getName() + " " + f.getType());
+        }
+        //endregion
+
 
         MainFrame window = new MainFrame(700, 500, chatClient);
         //test();
@@ -33,4 +49,14 @@ public class Main {
 
         client.logout();
     };
+
+    private static List<Field> getAllFields(Class<?> cls)
+    {
+        List<Field> fields = new ArrayList<>();
+        for(Field f: cls.getDeclaredFields())
+        {
+            fields.add(f);
+        }
+        return fields;
+    }
 }
